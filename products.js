dc2475a8b594be06cd4bd58d14646b9735451621
Wary <82594183+WaryMars@users.noreply.github.com>
2023-03-30 16:38:03 +0400

    Add files via upload

--- products.js
+++ products.js
@@ -15,7 +15,7 @@ ipcRenderer.on('product-list', (event, rows) => {
     for (var i = 0; i < rows.length; i++) {
         const row = "<tr><td>" + rows[i].id + "</td><td>" + rows[i].nom_prod + "</td><td>" + rows[i].prix + "</td><td>" + rows[i].stock + "</td><td>" + 
         " <button type='button' class='btn btn-primary' onclick='ModLigne(this)' data-bs-toggle='modal' data-bs-target='#updateModal' class='btn-del' style='border: none; background-color: white;' ><img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAN1JREFUSEvFlO0NwjAMBa8bwCQwAkzCKjACE8EIsAkbgJ6USKEkdj5a0T+V2vQufnY6sfI1rcxnRLABToDuV+CV22yvQNAbsA/QB3DMSXoEKfwZBDsgK2kVzOGHILgDklyAcxpVi6AUi3gxrm6BFYsE6oXiUkVfza6pwItFgixcLzxBDq4dlp7/TKolGIZbFSwCLwkWg5cEOjCa6bRx1ZnPm5DrwTss2oaR64aXKogCyYfgniCttjjn3u/eiih+2w2vOWjeBt333kl2Ad6Cmog8hjmZfxG07thcv3oPPg35Shmq75wnAAAAAElFTkSuQmCC' alt='Modifier'/></button> " +
-        " <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#delModal' class='btn-del' style='border: none; background-color: white;'> <img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAKlJREFUSEvtlcERQDAQRZ9KlIBOlEIFlKQUOqASJjM4hJ0fIje5ZTbZ9//uJJuReGWJ86MANdABpSFkBlpgsIQqgEuQC5cjUL0FrPtFS4iKyxKpBCp+ARwXYnt/OvatJwccyqV1z6J5/nXzvgb4CtX+5Ic6UAmjS/QDLu/qaUn+HsivKbpEivAYEDJofOh0N/msl+xGZQ8USvoeX4DmbnSqkRmY3z6WHLABUDk4GfuOp84AAAAASUVORK5CYII='/> </button> " + "</td></tr>";
+        " <button type='button' class='btn btn-primary' onclick='SuppLigne(this)' data-bs-toggle='modal' data-bs-target='#delModal' class='btn-del' style='border: none; background-color: white;'> <img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAKlJREFUSEvtlcERQDAQRZ9KlIBOlEIFlKQUOqASJjM4hJ0fIje5ZTbZ9//uJJuReGWJ86MANdABpSFkBlpgsIQqgEuQC5cjUL0FrPtFS4iKyxKpBCp+ARwXYnt/OvatJwccyqV1z6J5/nXzvgb4CtX+5Ic6UAmjS/QDLu/qaUn+HsivKbpEivAYEDJofOh0N/msl+xGZQ8USvoeX4DmbnSqkRmY3z6WHLABUDk4GfuOp84AAAAASUVORK5CYII='/> </button> " + "</td></tr>";
         document.getElementById("myTable").innerHTML += row;
     }
 });
@@ -31,6 +31,13 @@ function ModLigne(lig){
     document.getElementById("stock").value = selectLig.cells[3].innerHTML;    
 }
 
+
+function SuppLigne(r) {
+    // Supprimer le contenu HTML d'un élément ici une lgne de la table
+    var i = r.parentNode.parentNode.rowIndex;
+    document.getElementById("myTable").deleteRow(i);
+    
+}
 
 // Ajouter un nouveau produit
 addProductForm.addEventListener('submit', (event) => {
