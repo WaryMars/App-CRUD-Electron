fdcbb7505ab6d9d277ed00909c872e352aa27011
Raphael-ESSOB <113003686+Oliwed-Raphael-ESSOB@users.noreply.github.com>
2023-04-12 08:06:28 +0400

    Delete App-CRUD-Electron-main3 directory

--- App-CRUD-Electron-main3/deconnexion.js
+++
@@ -1,9 +0,0 @@
-const logoutButton = document.querySelector('#logout-button');
-
-logoutButton.addEventListener('click', () => {
-  // Envoyer un message pour effectuer la déconnexion
-  ipcRenderer.send('logout');
-
-  // Rediriger l'utilisateur vers la page de connexion
-  window.loadFile('connexion.html');
-});
